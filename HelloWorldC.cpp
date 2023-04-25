a0a554833491c44767c34a799fe4d91be3705863
UnpaidTax <illusion73@gmail.com>
2023-04-25 13:20:20 +0900

    3nd

--- HelloWorldC.cpp
+++ HelloWorldC.cpp
@@ -1,6 +1,17 @@
 #include <stdio.h>
 int main(void)
 {
+	
 	printf("Hello world! \n");
-	return true;
+
+	int num = 0;
+
+	while (1)
+	{
+		printf("숫자하나 입력 하세요 : ");
+		scanf_s("%d", &num);
+		printf("입력된 숫자는 %d입니다.\n ", (int)num);
+		if (num == 0) return 0;
+	}
+	return 1;
 }
